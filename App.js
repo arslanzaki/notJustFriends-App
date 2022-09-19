4b140fec09f073331cdaba4741faf9a8b0b3e772
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-09-19 19:34:08 +0500

    Create Post Screen

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import CreatePostScreen from "./src/screens/CreatePostScreen";
 
 import FeedScreen from "./src/screens/FeedScreen";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <FeedScreen />
+      {/* <FeedScreen /> */}
+      <CreatePostScreen />
     </View>
   );
 };
